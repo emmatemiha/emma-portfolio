dc0ee5c373560ced331693fe6b955cc1e8d30779
Emma <ete42@uclive.ac.nz>
2026-06-23 19:42:41 +1200

    fixed where the images are located and how they're referenced

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,11 @@
+import avatarImage from "../assets/images/avatar.svg";
+import htmlIcon from "../assets/icons/html-5.svg";
+import javascriptIcon from "../assets/icons/javascript.svg";
+import pythonIcon from "../assets/icons/python.svg";
+import reactIcon from "../assets/icons/react.svg";
+import tailwindIcon from "../assets/icons/tailwind.svg";
+import typescriptIcon from "../assets/icons/typescript.svg";
+
 export default function Home() {
     return (
         <div className="pt-24 px-8 min-h-screen max-w-5xl mx-auto">
@@ -18,7 +26,7 @@ export default function Home() {
                 </div>
 
                 <div className="flex-shrink-0">
-                    <img src="/icons/avatar.svg" alt="Emma"
+                    <img src={avatarImage} alt="Emma"
                         className="w-64 h-64 rounded-full object-cover border-4 border-burgundy/30 shadow-[0_2px_6px_rgba(0,0,0,0.12)]"
                     />
                 </div>
@@ -29,12 +37,12 @@ export default function Home() {
             <h1 className="text-3xl text-center font-serif text-burgundy mb-9">Tech Stack</h1>
 
             <div className="flex justify-center items-center gap-6">
-                <img src="/icons/python.svg" alt="Python" className="w-15 h-15" />
-                <img src="/icons/javascript.svg" alt="JavaScript" className="w-15 h-15" />
-                <img src="/icons/typescript.svg" alt="TypeScript" className="w-15 h-15" />
-                <img src="/icons/html-5.svg" alt="HTML" className="w-15 h-15" />
-                <img src="/icons/tailwind.svg" alt="Tailwind CSS" className="w-15 h-15" />
-                <img src="/icons/react.svg" alt="React" className="w-15 h-15" />
+                <img src={pythonIcon} alt="Python" className="w-15 h-15" />
+                <img src={javascriptIcon} alt="JavaScript" className="w-15 h-15" />
+                <img src={typescriptIcon} alt="TypeScript" className="w-15 h-15" />
+                <img src={htmlIcon} alt="HTML" className="w-15 h-15" />
+                <img src={tailwindIcon} alt="Tailwind CSS" className="w-15 h-15" />
+                <img src={reactIcon} alt="React" className="w-15 h-15" />
             </div>
         </div>
     )
